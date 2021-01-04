0417f273dce47db1d81875830bf12eb89965f6e5
Tyler Blovat <tyler.blovat@uconn.edu>
2021-01-04 13:04:51 -0500

    Restyling header for crunchlist page

--- components/product_pages/crunchlist/CrunchListPage/CrunchListPage.tsx
+++ components/product_pages/crunchlist/CrunchListPage/CrunchListPage.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Title } from "../Title/Title";
 import styles from "./styles.module.scss";
 
 export const CrunchListPage: React.FC = () => {
   return (
     <div className={styles.crunchlist_page}>
-      <p>CrunchListPage</p>
+      <Title />
     </div>
   );
 };
