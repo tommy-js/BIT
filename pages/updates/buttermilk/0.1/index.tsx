62e0e050ae813babaa64c2cb5cad1b786426f3e9
Tyler Blovat <tyler.blovat@uconn.edu>
2021-01-14 14:14:20 -0500

    Add updates route to pages

---
+++ pages/updates/buttermilk/0.1/index.tsx
@@ -0,0 +1,11 @@
+import { NavBar } from "../../../../components/navigation/NavBar/NavBar";
+import { Update01Page } from "../../../../components/updates/buttermilk/Update01Page/Update01Page";
+
+export default function Update01() {
+  return (
+    <div>
+      <NavBar />
+      <Update01Page />
+    </div>
+  );
+}
