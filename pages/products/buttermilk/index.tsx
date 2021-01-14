558f56ded151d74e225421b51083b7b58a41cf0e
Tyler Blovat <tyler.blovat@uconn.edu>
2021-01-14 16:53:23 -0500

    Add buttermilk product page

---
+++ pages/products/buttermilk/index.tsx
@@ -0,0 +1,11 @@
+import { NavBar } from "../../../components/navigation/NavBar/NavBar";
+import { ButtermilkPage } from "../../../components/product_pages/buttermilk/ButtermilkPage/ButtermilkPage";
+
+export default function Buttermilk() {
+  return (
+    <div>
+      <NavBar />
+      <ButtermilkPage />
+    </div>
+  );
+}
